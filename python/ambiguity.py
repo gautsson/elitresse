fa54585b78490f4a5070b2b9cda309717f4fb76d
Axner <axner@student.chalmers.se>
2014-04-07 15:47:41 +0200

    amgiguity methods implemented with andreea

--- python/ambiguity.py
+++ python/ambiguity.py
@@ -1,3 +1,5 @@
+import math
+
 class AmbiguityResolver:
     
     def __init__(self, goals, world):
@@ -11,26 +13,34 @@ class AmbiguityResolver:
         #secondObject = getObjectIndex(somethingElse)
         pass
     def onTop(self, topObject, botObject):
-        if top[0] == bot[0] and top[1] < bot[1]:
-            return true
-        return false
+        return topObject[0] == botObject[0] and topObject[1]-1 == botObject[1]
+    
     def inside(self, object, container):
-        pass
-    def rightOf(self, leftObject, rightObject):
-        pass
-    def leftOf(self, leftObject, rightObject):
-        pass
+        return onTop(object, container) #TODO IF WE NEED TO CHECK MORE
+    
+    def rightOf(self, right, left):
+        return right[0] > left[0]
+    
+    def leftOf(self, left, right):
+        return left[0] < right[0]
+    
     def above(self, topObject, botObject):
-        pass
-    def under(self, topObject, botObject):
-        pass
+        return topObject[1] > botObject[1]
+    
+    def under(self, botObject, topObject):
+        return botObject < topObject
     def beside(self, object, nextObject):
-        pass
-    def getObjectIndex(self, object):
-        for worldIndex in self.population:
+        return math.fabs(object[0]-nextObject[0]) == 1
+    
+    def getObjectCoordinates(self, object):
+        wIndex = 0
+        for worldIndex in self.worldPopulation:
+            sIndex = 0
             for stackIndex in worldIndex:
                 if stackIndex == object:
-                    return [worldIndex,stackIndex]
+                    return [wIndex,sIndex]
+                sIndex = sIndex + 1
+            wIndex = wIndex + 1
                 
     
     def getMatchingObjects(self, objectsToBeChecked):
@@ -91,15 +101,78 @@ class World:
         "k": { "form":"box",     "size":"large",  "color":"yellow"},
         "l": { "form":"box",     "size":"large",  "color":"red"   },
         "m": { "form":"box",     "size":"small",  "color":"blue"} }
+        
+        elif size == "complex":
+            self.population = [["e"],["a","l"],["i","h","j"],
+                                    ["c","k","g","b"],["d","m","f"]]
+            
+            self.worldObjects = {
+        "a": { "form":"brick",   "size":"large",  "color":"yellow" },
+        "b": { "form":"brick",   "size":"small",  "color":"white" },
+        "c": { "form":"plank",   "size":"large",  "color":"red"   },
+        "d": { "form":"plank",   "size":"small",  "color":"green" },
+        "e": { "form":"ball",    "size":"large",  "color":"white" },
+        "f": { "form":"ball",    "size":"small",  "color":"black" },
+        "g": { "form":"table",   "size":"large",  "color":"blue"  },
+        "h": { "form":"table",   "size":"small",  "color":"red"   },
+        "i": { "form":"pyramid", "size":"large",  "color":"yellow"},
+        "j": { "form":"pyramid", "size":"small",  "color":"red"   },
+        "k": { "form":"box",     "size":"large",  "color":"yellow"},
+        "l": { "form":"box",     "size":"large",  "color":"red"   },
+        "m": { "form":"box",     "size":"small",  "color":"blue"} }
+            
+        elif size == "impossible":
+            self.population = [["lbrick1","lball1","sbrick1"], [],
+                               ["lpyr1","lbox1","lplank2","sball2"], [],
+                               ["sbrick2","sbox1","spyr1","ltable1","sball1"]]
+            self.worldObjects = {
+        "lbrick1": { "form":"brick",   "size":"large",  "color":"green" },
+        "sbrick1": { "form":"brick",   "size":"small",  "color":"yellow" },
+        "sbrick2": { "form":"brick",   "size":"small",  "color":"blue" },
+        "lplank1": { "form":"plank",   "size":"large",  "color":"red"   },
+        "lplank2": { "form":"plank",   "size":"large",  "color":"black"   },
+        "splank1": { "form":"plank",   "size":"small",  "color":"green" },
+        "lball1":  { "form":"ball",    "size":"large",  "color":"white" },
+        "sball1":  { "form":"ball",    "size":"small",  "color":"black" },
+        "sball2":  { "form":"ball",    "size":"small",  "color":"red" },
+        "ltable1": { "form":"table",   "size":"large",  "color":"green"  },
+        "stable1": { "form":"table",   "size":"small",  "color":"red"   },
+        "lpyr1":   { "form":"pyramid", "size":"large",  "color":"white"},
+        "spyr1":   { "form":"pyramid", "size":"small",  "color":"blue"   },
+        "lbox1":   { "form":"box",     "size":"large",  "color":"yellow"},
+        "sbox1":   { "form":"box",     "size":"small",  "color":"red"   },
+        "sbox2":   { "form":"box",     "size":"small",  "color":"blue"} }
             
 class Tester:
     
     def __init__(self, testAtt):
-        self.oneAtt = testAtt
-        print "made a class and its attribute is ", self.oneAtt
+        pass
+        #self.oneAtt = testAtt
+        #print "made a class and its attribute is ", self.oneAtt
     
         
 if __name__ == '__main__':
+    mySmallWorld = World("small")
+    ambSmallSolver = AmbiguityResolver("someGoal", mySmallWorld)
+    f = ambSmallSolver.getObjectCoordinates("f")
+    m = ambSmallSolver.getObjectCoordinates("m")
+    print "----Should be True, False all the time-----"
+    print ambSmallSolver.onTop(f, m)
+    print ambSmallSolver.onTop(m, f)
+    e = ambSmallSolver.getObjectCoordinates("e")
+    print ambSmallSolver.rightOf(f, e)
+    print ambSmallSolver.rightOf(m, f)
+    print ambSmallSolver.leftOf(e, f)
+    print ambSmallSolver.leftOf(m, e)
+    print ambSmallSolver.above(f, e)
+    l = ambSmallSolver.getObjectCoordinates("l")
+    print ambSmallSolver.above(l, m)
+    print ambSmallSolver.under(e, f)
+    print ambSmallSolver.under(f, l)
+    print ambSmallSolver.beside(e, l)
+    print ambSmallSolver.beside(l, e)
+    print ambSmallSolver.beside(m, l)
+    """
     mySmallWorld = World("small")
     myMediumWorld = World("medium")
     ambSmallSolver = AmbiguityResolver("someGoal", mySmallWorld)
@@ -122,6 +195,6 @@ if __name__ == '__main__':
     print "Testing medium-sized world"
     print "It should print: [[],[k,lm,l],[d],[b,e]]"
     print "Matches found: ", mediumMatches
-    
+    """
         
         
