036281b921605bcc6a8af7d328fc76f1d2ee25f6
analoganalogy <robert@hotmail.com>
2014-04-07 13:31:11 +0200

    Interpreter, distinguish between the three different commands

--- python/interpret.py
+++ python/interpret.py
@@ -1,11 +1,54 @@
-class interpreter(parse_trees):
+class Interpreter:
     
-    #def __init__(self, parse_trees):         
-    def interpret(parse_trees, world, objects):
+                   
+    def __init__(self):
+        None
+        
+    def interpret(self, parse_trees):
+        goals = list()
+        
         for eachParseTree in parse_trees:
-            action  = eachParseTree[0]
-            src     = eachParseTree[1]
-            dst     = eachParseTree[2]
+            command = eachParseTree[0]
         
+            if command == "take":
+                print "take"
+                entity = eachParseTree[1]
+                
+                #entity = interpretEntity(entity)
+            
+            elif command == "put":
+                print 'put'
+                location = eachParseTree[1]
+                
+                location = interpretLocation(location)
+            
+            elif command == "move":
+                print "move"
+                entity   = eachParseTree[1]
+                location = eachParseTree[2]
+                
+                entity   = interpretEntity(entity)
+                location = interpretLocation(location)
         
-
+            
+    def interpretEntity(self, entity):
+        print entity
+        
+    def interpretLocation(self, location):  
+        print location  
+ 
+    """ Quantifiers
+       the
+       a | an | any
+       every
+       all
+       
+       Relations
+       beside
+       leftOf
+       rightOf
+       above
+       onTop
+       under
+       inside
+    """
