cae2c7131a598ea2fbfb31a977d73d66f5674f5d
analoganalogy <robert@hotmail.com>
2014-04-11 13:46:09 +0200

    Changed commandline

--- python/shrd.py
+++ python/shrd.py
@@ -1,47 +1,18 @@
 import shrdlite
+import interpret
+import ambiguity
 
 def getInput():	
-    input = raw_input("\nEnter a command for SHRDLITE:\n\n")
+    input = raw_input("\nEnter a command for SHRDLITE:")
     return input.split()
-	
-def searchBasicEntity(usrInput, world):
-    objlist = []
-    for worldObj in world:
-        ok = True
-        if usrInput[0] != "anyform" and usrInput[0] != world[worldObj]['form']:
-            ok = False
-        if usrInput[1] and usrInput[1] != world[worldObj]['size']:
-            ok = False
-        if usrInput[2] and usrInput[2] != world[worldObj]['color']:
-            ok = False
-        if ok:
-            objlist.append(worldObj)
-    return objlist
-
-def interpret():
-    world = {
-        "a": { "form":"brick",   "size":"large",  "color":"green" },
-        "b": { "form":"brick",   "size":"small",  "color":"white" },
-        "c": { "form":"plank",   "size":"large",  "color":"red"   },
-        "d": { "form":"plank",   "size":"small",  "color":"green" },
-        "e": { "form":"ball",    "size":"large",  "color":"white" },
-        "f": { "form":"ball",    "size":"small",  "color":"black" },
-        "g": { "form":"table",   "size":"large",  "color":"blue"  },
-        "h": { "form":"table",   "size":"small",  "color":"red"   },
-        "i": { "form":"pyramid", "size":"large",  "color":"yellow"},
-        "j": { "form":"pyramid", "size":"small",  "color":"red"   },
-        "k": { "form":"box",     "size":"large",  "color":"yellow"},
-        "l": { "form":"box",     "size":"large",  "color":"red"   },
-        "m": { "form":"box",     "size":"small",  "color":"yellow"  }
-    }
-    objlist = searchBasicEntity(["box","small","yellow"], world)
-    for obj in objlist:
-        print obj
-
     
-
 if __name__ == '__main__':
-    x = getInput()
-    y = shrdlite.parse(x)
-    print y
-    interpret()
+    utterance = getInput()
+    parse_trees = shrdlite.parse(utterance)
+    print parse_trees
+    
+    x = interpret.Interpreter()
+    
+    goals = x.interpret(parse_trees)
+    
+    
