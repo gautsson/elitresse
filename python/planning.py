98b3025579fff73b68ef2c02b63b88ca02ef0d67
Thor <thgautsson@gmail.com>
2014-04-12 04:03:30 +0200

    Some functions

--- python/planning.py
+++ python/planning.py
@@ -1,6 +1,23 @@
-# The world and the goal
-world = [["e"],["g","l"],[],["k","m","f"],[]]
-holding = null
+world = [["e"],["a","l"],[],[],["i","h","j"],[],[],["k","g","c","b"],[],["d","m","f"]]
+objects = {
+        "a": { "form":"brick",   "size":"large",  "color":"green" },
+        "b": { "form":"brick",   "size":"small",  "color":"white" },
+        "c": { "form":"plank",   "size":"large",  "color":"red"   },
+        "d": { "form":"plank",   "size":"small",  "color":"green" },
+        "e": { "form":"ball",    "size":"large",  "color":"white" },
+        "f": { "form":"ball",    "size":"small",  "color":"black" },
+        "g": { "form":"table",   "size":"large",  "color":"blue"  },
+        "h": { "form":"table",   "size":"small",  "color":"red"   },
+        "i": { "form":"pyramid", "size":"large",  "color":"yellow"},
+        "j": { "form":"pyramid", "size":"small",  "color":"red"   },
+        "k": { "form":"box",     "size":"large",  "color":"yellow"},
+        "l": { "form":"box",     "size":"large",  "color":"red"   },
+        "m": { "form":"box",     "size":"small",  "color":"blue"} }
+
+test = {
+        "a": { "form":"brick",   "size":"large",  "color":"green" }}
+holding = False
+
 
 # Gets all the empty stacks in the world
 def getEmptyStacks(world):
@@ -10,9 +27,73 @@ def getEmptyStacks(world):
 			x.append(stack)
 	return x
 
+# Gets the top objects on the stacks which are in the world
+def getTopObject(world):
+	balls = []
+	for object in world:
+		if object != []:
+ 			balls.append(object[-1])
+ 		else:
+ 			balls.append([])
+ 	return balls
 
-		
-	
+# Gets an ordered list of all the objects in the world
+def getOrderedListOfObjects(world):
+	orderedList = [item for sublist in world for item in sublist]
+	orderedList.sort()
+	return orderedList
+
+# Gets the stack positions of all the balls which are on the top of a stack
+# (There's a rule that no objects are allowed to be put upon balls)
+def getStacksWithBallsOnTop(world):
+	topBalls = []
+	topBallStacks = []
+	topObjectsInWorld = getTopObject(world)
+	for object in topObjectsInWorld:
+		if object != []:
+			a = ''.join(object)
+			objectForm = objects[a].get("form")
+			if objectForm == "ball":
+				topBalls.append(objectForm)
+			else:
+				topBalls.append([])
+		else: 
+			topBalls.append([])
+	for stack, item in enumerate(topBalls):
+		if item is "ball":
+			topBallStacks.append(stack)
+	return topBallStacks
+
+# Gets the stack positions where small objects are on top of the stack
+# (Small objects cannot hold large objects)
+def getStacksWithSmallObjectsOnTop(world):
+	smallObjects = []
+	topSmallObjectStacks = []
+	topObjectsInWorld = getTopObject(world)
+	for object in topObjectsInWorld:
+		if object != []:
+			a = ''.join(object)
+			objectForm = objects[a].get("size")
+			if objectForm == "small":
+				smallObjects.append(objectForm)
+			else:
+				smallObjects.append([])
+		else: 
+			smallObjects.append([])
+	for stack, item in enumerate(smallObjects):
+		if item is "small":
+			topSmallObjectStacks.append(stack)
+	return topSmallObjectStacks
+
+
 
 if __name__ == '__main__':
-    	print(getEmptyStacks(world))
+	# a = (ontop, e, g)
+	# print(objects)
+
+	#print getEmptyStacks(world)
+	#print getTopObject(world)
+	#print getOrderedListOfObjects(world)
+
+	print getStacksWithBallsOnTop(world)
+	print getStacksWithSmallObjectsOnTop(world)
